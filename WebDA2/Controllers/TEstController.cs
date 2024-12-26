2c9ee0a29c23399064fefc848f7bb19fe6a73ac5
thanhdat-24 <thanhdat28235@gmail.com>
2024-12-26 17:44:10 +0700

    thử code

---
+++ WebDA2/Controllers/TEstController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebDA2.Controllers
+{
+    public class TEstController : Controller
+    {
+        // GET: TEst
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
